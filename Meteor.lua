b24f9242240004b11d821ba9e54800a62f62acff
NovaPlays <luka.verhulst2@gmail.com>
2023-02-21 23:14:41 +0100

    Update Meteor.lua

--- Meteor.lua
+++ Meteor.lua
@@ -8,6 +8,78 @@
 util.keep_running()
 util.require_natives(1663599433)
 
+-----------------
+--update system--
+-----------------
+local status, auto_updater = pcall(require, "auto-updater")
+if not status then
+    local auto_update_complete = nil util.toast("Installing auto-updater...", TOAST_ALL)
+    async_http.init("raw.githubusercontent.com", "/hexarobi/stand-lua-auto-updater/main/auto-updater.lua",
+        function(result, headers, status_code)
+            local function parse_auto_update_result(result, headers, status_code)
+                local error_prefix = "Error downloading auto-updater: "
+                if status_code ~= 200 then util.toast(error_prefix..status_code, TOAST_ALL) return false end
+                if not result or result == "" then util.toast(error_prefix.."Found empty file.", TOAST_ALL) return false end
+                filesystem.mkdir(filesystem.scripts_dir() .. "lib")
+                local file = io.open(filesystem.scripts_dir() .. "lib\\auto-updater.lua", "wb")
+                if file == nil then util.toast(error_prefix.."Could not open file for writing.", TOAST_ALL) return false end
+                file:write(result) file:close() util.toast("Successfully installed auto-updater lib", TOAST_ALL) return true
+            end
+            auto_update_complete = parse_auto_update_result(result, headers, status_code)
+        end, function() util.toast("Error downloading auto-updater lib. Update failed to download.", TOAST_ALL) end)
+    async_http.dispatch() local i = 1 while (auto_update_complete == nil and i < 40) do util.yield(250) i = i + 1 end
+    if auto_update_complete == nil then error("Error downloading auto-updater lib. HTTP Request timeout") end
+    auto_updater = require("auto-updater")
+end
+if auto_updater == true then error("Invalid auto-updater lib. Please delete your Stand/Lua Scripts/lib/auto-updater.lua and try again") end
+
+local default_check_interval = 604800
+local auto_update_config = {
+    source_url="https://raw.githubusercontent.com/NovaPlays134/Meteor/main/Meteor.lua",
+    script_relpath=SCRIPT_RELPATH,
+    switch_to_branch=selected_branch,
+    verify_file_begins_with="--",
+    check_interval=86400,
+    silent_updates=true,
+    dependencies={
+        {
+            name="ent_functions",
+            source_url="https://raw.githubusercontent.com/NovaPlays134/Meteor/main/lib/Meteor/Functions/ent_functions.lua",
+            script_relpath="lib/Meteor/Functions/ent_functions.lua",
+            check_interval=default_check_interval,
+        },
+
+        {
+            name="functions",
+            source_url="https://raw.githubusercontent.com/NovaPlays134/Meteor/main/lib/Meteor/Functions/functions.lua",
+            script_relpath="lib/Meteor/Functions/functions.lua",
+            check_interval=default_check_interval,
+        },
+
+        {
+            name="playr_functions",
+            source_url="https://raw.githubusercontent.com/NovaPlays134/Meteor/main/lib/Meteor/Functions/playr_functions.lua",
+            script_relpath="lib/Meteor/Functions/playr_functions.lua",
+            check_interval=default_check_interval,
+        },
+
+        {
+            name="weapon_hashes",
+            source_url="https://raw.githubusercontent.com/NovaPlays134/Meteor/main/lib/Meteor/Functions/weapon_hashes.lua",
+            script_relpath="lib/Meteor/Functions/weapon_hashes.lua",
+            check_interval=default_check_interval,
+        },
+
+        {
+            name="Playerblip",
+            source_url="https://raw.githubusercontent.com/NovaPlays134/Meteor/main/lib/Meteor/sprite1.png",
+            script_relpath="lib/Meteor/sprite1.png",
+            check_interval=default_check_interval,
+        },
+    }
+}
+auto_updater.run_auto_update(auto_update_config)
+
 --requiring files--
 local func = require "Meteor.Functions.functions"
 local ent_func = require "Meteor.Functions.ent_functions"
